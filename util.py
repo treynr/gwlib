3028e0e71364812351f28c298c2a042a0014b6cb
TR <git@fin0c.net>
2016-06-08 12:46:50 -0500

    Refactor util and remove extraneous functions

--- util.py
+++ util.py
@@ -1,8 +1,8 @@
-#!/usr/bin/python
+#!/usr/bin/env python2
 
 ## file:    util.py
 ## desc:    A bunch of misc. utility functions.
-## vers:    0.1
+## vers:    0.4.0
 ## auth:    TR
 # 
 
@@ -10,106 +10,62 @@ from collections import defaultdict as dd
 import datetime as dt
 import json
 
-## tup2dict
-#
-## Finally fucking wrote this because I need this functionality quite often 
-## and writing a for loop every time was getting annoying.
-## Converts a list of tuples into a dict. Use key to specifiy which tuple 
-## index (0/1) should be used as the key (only supports two-element tuples 
-## right now). If lst == True, then the dictionary values are treated as 
-## lists instead of just some single variable.  
-#
-## arg, tup, list of tuples that will converted to a dict
-## arg, key, tuple index to use as the key
-## arg, lst, specifies if dict values should be treated as lists 
-## ret, dictionary
-#
-def tup2dict(tup, key=0, lst=False):
-	val = 0 # Tuple index to use as values
+def chunk_list(l, n):
+    """
+    Chunks a list into a list of list where each sublist has a size of n.
 
-	if key == 0:
-		val = 1
-	if lst:
-		tmap = dd(list)
-	else:
-		tmap = {}
-	
-	for t in tup:
-		if lst:
-			tmap[t[key]].append(t[val])
-		else:
-			tmap[t[key]] = t[val]
+    :type l: list
+    :arg l: the list being chunked
 
-	return tmap
+    :ret list: chunks
+    """
 
-## splitList
-#
-## Splits a comma separated string into a list. Removes any extraneous spaces
-## from each word too.
-#
-## arg, strs, string to split
-## ret, list of words
-#
-def splitList(strs):
-    if strs is None: 
-        return None
-    return [l.strip() for l in strs.split(',')] 
-
-## chunkList
-#
-## Takes a list and chunks it into lists of size n. Use n == 0 to specify
-## a chunk the size of the initial list.
-#
-## arg, l, the list to chunk
-## arg, n, the size of the newly generated sublists/chunks
-## ret, list of lists
-#
-def chunkList(l, n):
 	if n == 0:
 		n = len(l)
 
 	for i in xrange(0, len(l), n):
 		yield l[i:i+n]
 
-#### flatten
-##
-#### Takes a list of lists (inner list lengths don't have to be equal) and 
-#### flattens everything so we just have one giant list of whatever shit 
-#### was in the inner lists.
-##
-def flatten(outlst):
-    return [a for inlst in outlst for a in inlst]
+def flatten(outlist):
+    """
+    Flattens a list of lists into a single list.
 
-#### flatten
-##
-#### This ugly fucker of a list comprehension just takes a list of lists 
-#### (inner list lengths don't have to be equal) and flattens everything so we
-#### just have one giant list of whatever shit was in the inner lists.
-##
-def flattenList(outlst):
-    return [a for inlst in outlst for a in inlst]
+    :type outlist: list
+    :arg outlist: 
 
-## exportJson
-#
-## Takes a filepath and some data structure (usually a list of objects) and 
-## dumps the data to a JSON file at the given path. 
-#
-## arg, fp, filepath to write to
-## arg, data, data to export
-## arg, dtag, OPTIONAL data tag string used to tag the data w/ script arguments
-#
-def exportJson(fp, data, dtag=''):
+    :ret list:
+    """
+
+    return [a for inlist in outlist for a in inlist]
+
+def export_json(fp, data, dtag=''):
+    """
+    Exports a python data structure into a JSON string and saves the result to
+    a given file.
+
+    :type fp: str
+    :arg fp: filepath
+
+    :type data: ?
+    :arg data: the shit being exported
+
+    :type dtag: str
+    :arg dtag: An optional tag prepended to the exported data
+    """
+
 	with open(fp, 'w') as fl:
 		if dtag == '':
 			print >> fl, json.dumps(data)
 		else:
 			print >> fl, json.dumps([dtag, data])
 
-#### getToday
-##
-#### Returns today's date as a string in the format YYYY-MM-DD.
-##
-def getToday():
+def get_today():
+    """
+    Returns today's date a string in the format YYYY.MM.DD.
+
+    :ret str: the date
+    """
+
 	now = dt.datetime.now()
 	year = str(now.year)
 	month = str(now.month)
@@ -117,6 +73,7 @@ def getToday():
 
 	if len(month) == 1:
 		month = '0' + month
+
 	if len(day) == 1:
 		day = '0' + day
 
