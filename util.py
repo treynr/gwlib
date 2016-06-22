81a93c5640820d5990fd473ef21152d656993c9e
TR <git@fin0c.net>
2016-06-22 14:10:01 -0500

    Add make_geneset function to util

--- util.py
+++ util.py
@@ -105,3 +105,77 @@ def parse_generic_file(fp, delim='\t'):
     
     return data
 
+def make_geneset(name, abbrev, desc, sp_id, pub_id, grps, score_type, thresh,
+                 gene_type, gene_vals, usr_id, cur_id=5):
+    """
+    Given a shitload of arguments, this function returns a dictionary
+    representation of a single geneset. Each key is a different column found
+    in the geneset table. Not all columns are (or need to be) represented.
+
+    :type name: str
+    :arg name: geneset name
+
+    :type abbrev: str
+    :arg abbrev: geneset abbreviation
+
+    :type desc: str
+    :arg desc: geneset description
+
+    :type sp_id: int
+    :arg sp_id: species ID
+
+    :type pub_id: int
+    :arg pub_id: publication ID
+
+    :type grps: str
+    :arg grps: comma separated list of group ids (grp_id)
+
+    :type score_type: int
+    :arg score_type: geneset value score type
+
+    :type thresh: float
+    :arg thresh: geneset value threshold
+
+    :type gene_type: int
+    :arg gene_type: gene ID type
+
+    :type gene_vals: list
+    :arg gene_vals: tuples of ode_gene_ids and their values
+
+    :type usr_id: int
+    :arg usr_id: user ID
+
+    :type cur_id: int
+    :arg cur_id: curation tier
+
+    :ret dict: 
+    """
+
+    gs = {}
+
+    ## If the geneset isn't private, neither should the group be
+    if cur_id != 5 and grps.find('-1'):
+        grps = grps.split(',')
+        grps = map(lambda x: if x == '-1': '0' else x, grps)
+        grps = ','.join(grps)
+
+    gs['gs_name'] = name
+    gs['gs_abbreviation'] = abbrev
+    gs['gs_description'] = desc
+    gs['sp_id'] = int(sp_id)
+    gs['gs_groups'] = grps
+    gs['pub_id'] = pub_id
+    gs['gs_threshold_type'] = int(score_type)
+    gs['gs_threshold'] = thresh
+    gs['gs_gene_id_type'] = int(gene_type)
+    gs['usr_id'] = int(usr_id)
+    ## Not a column in the geneset table; but these are processed later since
+    ## each geneset_value requires a gs_id
+    gs['geneset_values'] = gene_vals
+
+    ## Other fields we can fill out
+    gs['gs_count'] = len(vals)
+    gs['cur_id'] = cur_id
+
+    return gs
+
