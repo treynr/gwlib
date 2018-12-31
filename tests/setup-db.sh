cc1a5d3c4d8dcbf5378f5927fa54a68a273db726
TR <git@fin0c.net>
2018-12-31 14:24:29 -0600

    Attempt to force travis ci to use postgres 10

--- tests/setup-db.sh
+++ tests/setup-db.sh
@@ -4,7 +4,7 @@
 ## desc: Sets up the postgres DB and users for testing.
 ## auth: TR
 
-sudo -u postgres psql -U postgres -c 'CREATE USER test_user;'
-sudo -u postgres psql -U postgres -c 'ALTER USER test_user WITH SUPERUSER;'
-sudo -u postgres psql -U postgres -c 'CREATE DATABASE test_db;'
-sudo -u postgres psql -U postgres test_db -f setup-db.sql
+psql -c 'CREATE USER test_user;'
+psql -c 'ALTER USER test_user WITH SUPERUSER;'
+psql -c 'CREATE DATABASE test_db;'
+psql test_db -f setup-db.sql
